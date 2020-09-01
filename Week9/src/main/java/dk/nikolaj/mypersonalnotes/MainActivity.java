6a66f5d5d3f1ee9793437186fdd758ac893d5089
Nikolaj <43247719+Nikolaj93@users.noreply.github.com>
2020-09-01 23:05:59 +0200

    My Personal Notes

---
+++ Week9/src/main/java/dk/nikolaj/mypersonalnotes/MainActivity.java
@@ -0,0 +1,60 @@
+package dk.nikolaj.mypersonalnotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import dk.nikolaj.mypersonalnotes.Adapter.MyRecycleViewAdapter;
+import dk.nikolaj.mypersonalnotes.Model.Note;
+import dk.nikolaj.mypersonalnotes.Storage.FirestoreRepo;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView notesList;
+    private MyRecycleViewAdapter adapter;
+    private LinearLayoutManager layoutManager;
+    private FirestoreRepo db;
+    private List<Note> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = FirestoreRepo.getInstance();
+
+        notesList = findViewById(R.id.notesList);
+        layoutManager = new LinearLayoutManager(this);
+        notesList.setLayoutManager(layoutManager);
+        adapter = new MyRecycleViewAdapter(notes);
+        notesList.setAdapter(adapter);
+        db.setChangeListener(notes, adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.note_list_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public void newNote(MenuItem menuItem) {
+        startActivity(new Intent(this, NoteEditActivity.class));
+    }
+}
---
+++ Week9/src/main/java/dk/nikolaj/mypersonalnotes/NoteEditActivity.java
@@ -0,0 +1,112 @@
+package dk.nikolaj.mypersonalnotes;
+
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dk.nikolaj.mypersonalnotes.Model.Note;
+import dk.nikolaj.mypersonalnotes.Storage.FirestoreRepo;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class NoteEditActivity extends AppCompatActivity {
+
+    public static final String NOTE_KEY = "NOTE";
+    private Note noteBeingEdited;
+    private TextView titleInput;
+    private TextView contentInput;
+    private FirestoreRepo db;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_edit);
+        this.titleInput = findViewById(R.id.titleInput);
+        this.contentInput = findViewById(R.id.contentInput);
+        this.db = FirestoreRepo.getInstance();
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(NOTE_KEY)) {
+            this.noteBeingEdited = (Note) intent.getSerializableExtra(NOTE_KEY);
+            titleInput.setText(this.noteBeingEdited.getTitle());
+            contentInput.setText(this.noteBeingEdited.getContent());
+        } else {
+            this.noteBeingEdited = new Note();
+        }
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.noteBeingEdited.setTitle(titleInput.getText().toString());
+                this.noteBeingEdited.setContent(contentInput.getText().toString());
+                if (this.noteBeingEdited.isNew()) {
+                    if (!(noteBeingEdited.getTitle().isEmpty() && noteBeingEdited.getContent().isEmpty())) {
+                        if (noteBeingEdited.getTitle().isEmpty()) {
+                            noteBeingEdited.setTitle("Untitled note");
+                        }
+                        db.addOrUpdateNote(this.noteBeingEdited);
+                    }
+                } else {
+                    db.addOrUpdateNote(this.noteBeingEdited);
+                }
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.note_edit_menu, menu);
+        if (noteBeingEdited.isNew()) {
+            MenuItem deleteOption = menu.findItem(R.id.deleteMenu);
+            if (deleteOption != null) {
+                deleteOption.setVisible(false);
+            }
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void deleteNote(MenuItem menuItem) {
+        db.deleteNote(noteBeingEdited);
+        finish();
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView =findViewById(R.id.photoView);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void cancel(MenuItem menuItem) {
+        finish();
+    }
+}
